9978b34eb98a4d7e5bd4654d68a0c7e4c571dc1d
tlopo <tiago@aw20.co.uk>
2018-01-06 21:41:28 +0100

    work in progress

--- orchestrator/terraform/modules/net/vpc.tf
+++ orchestrator/terraform/modules/net/vpc.tf
@@ -5,6 +5,17 @@ resource "aws_vpc" "vpc" {
   enable_dns_hostnames = "true"
   cidr_block = "${var.cidr}"
   tags = {
-    Name = "vpc-${var.environment}" 
+    Name = "vpc-${var.environment}-vpc" 
   }
 }
+
+resource "aws_subnet" "default" {
+  vpc_id = "${aws_vpc.vpc.id}"
+  cidr_block = "${var.cidr}"
+  tags = {
+    Name = "#{var.environment}-net"
+  }
+}
+
+output "vpc-id" { value = "${aws_vpc.vpc.id}" }
+output "net-id" { value = "${aws_subnet.default.id}" }
